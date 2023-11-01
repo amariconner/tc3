a2a0b4d26947de29f66595efcdbe53b8eb97057a
amariconner <145292106+amariconner@users.noreply.github.com>
2023-11-01 14:10:08 -0500

    initial commit

---
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,14 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Adding",p:["p1"]},{w:"Zoom",p:["p1","p2","p4","p5","p6","p7"]},{w:"to",p:["p1","p2"]},{w:"Canvas",p:["p1","p4","p6"]},{w:"Courses",p:["p1"]},{w:"When",p:["p2"]},{w:"Use",p:["p2"]},{w:"vs",p:["p2"]},{w:"Google",p:["p2"]},{w:"Hangouts",p:["p2"]},{w:"Meet",p:["p2"]},{w:"Creating",p:["p3"]},{w:"Meeting",p:["p3","p4"]},{w:"Breakout",p:["p3"]},{w:"Rooms",p:["p3"]},{w:"Host",p:["p4"]},{w:"a",p:["p4"]},{w:"Within",p:["p4"]},{w:"Teach,",p:["p5"]},{w:"Meet,",p:["p5"]},{w:"and",p:["p5"]},{w:"Learn",p:["p5"]},{w:"with",p:["p5"]},{w:"for",p:["p6"]},{w:"Overview",p:["p6"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5, 6, 7];
+ph["p2"]=[8, 5, 9, 4, 10, 11, 12, 13];
+ph["p3"]=[14, 15, 16, 17];
+ph["p4"]=[18, 19, 4, 15, 20, 6];
+ph["p5"]=[21, 22, 23, 24, 25, 4];
+ph["p6"]=[4, 26, 6, 27];
+ph["p7"]=[4];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {6:-1,3:6,2:5,5:-1,1:6,4:6};
