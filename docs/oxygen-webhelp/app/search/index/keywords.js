1c9af54900e1ad24f42d86fd78c24ac79551121b
amariconner <145292106+amariconner@users.noreply.github.com>
2023-11-04 16:10:23 -0500

    final commit

--- docs/oxygen-webhelp/app/search/index/keywords.js
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -1,13 +1,14 @@
-var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Adding",p:["p1"]},{w:"Zoom",p:["p1","p2","p4","p5","p6","p7"]},{w:"to",p:["p1","p2"]},{w:"Canvas",p:["p1","p4","p6"]},{w:"Courses",p:["p1"]},{w:"When",p:["p2"]},{w:"Use",p:["p2"]},{w:"vs",p:["p2"]},{w:"Google",p:["p2"]},{w:"Hangouts",p:["p2"]},{w:"Meet",p:["p2"]},{w:"Creating",p:["p3"]},{w:"Meeting",p:["p3","p4"]},{w:"Breakout",p:["p3"]},{w:"Rooms",p:["p3"]},{w:"Host",p:["p4"]},{w:"a",p:["p4"]},{w:"Within",p:["p4"]},{w:"Teach,",p:["p5"]},{w:"Meet,",p:["p5"]},{w:"and",p:["p5"]},{w:"Learn",p:["p5"]},{w:"with",p:["p5"]},{w:"for",p:["p6"]},{w:"Overview",p:["p6"]}];
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Adding",p:["p1"]},{w:"Zoom",p:["p1","p2","p4","p5","p6","p7","p8"]},{w:"to",p:["p1","p7"]},{w:"Canvas",p:["p1","p4","p6"]},{w:"Courses",p:["p1"]},{w:"Features",p:["p2"]},{w:"of",p:["p2"]},{w:"and",p:["p2","p5"]},{w:"Google",p:["p2","p7"]},{w:"Hangouts",p:["p2","p7"]},{w:"Meet",p:["p2","p7"]},{w:"Creating",p:["p3"]},{w:"Meeting",p:["p3","p4"]},{w:"Breakout",p:["p3"]},{w:"Rooms",p:["p3"]},{w:"Hosting",p:["p4"]},{w:"a",p:["p4"]},{w:"Within",p:["p4"]},{w:"Teach,",p:["p5"]},{w:"Meet,",p:["p5"]},{w:"Learn",p:["p5"]},{w:"with",p:["p5"]},{w:"for",p:["p6"]},{w:"Overview",p:["p6"]},{w:"When",p:["p7"]},{w:"Use",p:["p7"]},{w:"vs",p:["p7"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
 ph["p1"]=[3, 4, 5, 6, 7];
-ph["p2"]=[8, 5, 9, 4, 10, 11, 12, 13];
+ph["p2"]=[8, 9, 4, 10, 11, 12, 13];
 ph["p3"]=[14, 15, 16, 17];
 ph["p4"]=[18, 19, 4, 15, 20, 6];
-ph["p5"]=[21, 22, 23, 24, 25, 4];
-ph["p6"]=[4, 26, 6, 27];
-ph["p7"]=[4];
+ph["p5"]=[21, 22, 10, 23, 24, 4];
+ph["p6"]=[4, 25, 6, 26];
+ph["p7"]=[27, 5, 28, 4, 29, 11, 12, 13];
+ph["p8"]=[4];
 var keywordsInfo = {
     keywords: keywords,
     ph: ph
--- docs/oxygen-webhelp/app/search/index/link-to-parent.js
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,2 +1,2 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-var linkToParent = {6:-1,3:6,2:5,5:-1,1:6,4:6};
+var linkToParent = {4:6,1:6,5:-1,7:5,2:5,6:-1,3:6};
